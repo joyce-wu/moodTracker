7470051267503911e77c779fa84cc0f7f015f0a1
Joyce Wu <jwu26@stuy.edu>
2019-11-20 14:04:16 -0500

    fdsfo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from utils import database
+from datetime import datetime
 import urllib2, json
 
 app = Flask(__name__)
@@ -11,8 +12,10 @@ def start():
     return render_template('main.html')
 
 ## mood graph over time
-@app.route("/mood_graph")
+@app.route("/mood_graph", methods=['GET', 'POST'])
 def graph():
+    date = datetime.now()
+    database.addMood(date, request.form["mood"])
     return render_template('moodgraph.html', mood = database.getMoods())
 
 ## gratitude journal
